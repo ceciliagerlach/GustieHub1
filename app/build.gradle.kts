710b9cbf83305b9f0f1537b65992dd8dad1c4c14
ceciliagerlach <gerlachcecilia@gmail.com>
2025-03-16 15:32:39 -0500

    WIP: Starting Navigation Drawer
    Tabbed navigation to display all groups that a user is a member of

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -42,6 +42,7 @@ dependencies {
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
+    implementation(libs.material.v1130alpha11)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
