a487478e5f393f89a324b5991b55701e98674f5b
ceciliagerlach <gerlachcecilia@gmail.com>
2025-03-23 18:23:37 -0500

    Resolving merging conflicts

--- app/src/main/java/com/example/gustiehub/GroupsActivity.kt
+++ app/src/main/java/com/example/gustiehub/GroupsActivity.kt
@@ -45,7 +45,7 @@ class GroupsActivity : AppCompatActivity() {
 
         groupsRecyclerView = findViewById(R.id.groupsRecyclerView)
         groupsRecyclerView.layoutManager = LinearLayoutManager(this)
-        groupsAdapter = GroupsAdapter(groupList,onItemClick = { selectedGroup ->
+        groupsAdapter = GroupsAdapter(groupList, onItemClick = { selectedGroup ->
             val intent = Intent(this, GroupsActivity::class.java)
             intent.putExtra("groupName", selectedGroup.name)
             startActivity(intent)
@@ -73,13 +73,13 @@ class GroupsActivity : AppCompatActivity() {
         }
 
         // list in discover groups
-        groupsRecyclerView = findViewById(R.id.groupsRecyclerView)
-        groupsRecyclerView.layoutManager = LinearLayoutManager(this)
-        groupsAdapter = GroupsAdapter(groupList) { selectedGroup ->
-            val intent = Intent(this, GroupsActivity::class.java)
-            intent.putExtra("groupName", selectedGroup.name)
-            startActivity(intent)
-        }
+        //groupsRecyclerView = findViewById(R.id.groupsRecyclerView)
+        //groupsRecyclerView.layoutManager = LinearLayoutManager(this)
+        //groupsAdapter = GroupsAdapter(groupList) { selectedGroup ->
+            //val intent = Intent(this, GroupsActivity::class.java)
+            //intent.putExtra("groupName", selectedGroup.name)
+            //startActivity(intent)
+        //}
         groupsRecyclerView.adapter = groupsAdapter
         GlobalData.getGroupList { updatedGroups ->
             runOnUiThread {
