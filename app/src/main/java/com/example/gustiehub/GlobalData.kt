ffd97047ce9e1b32cf91ca7fa9d2940179f643e4
ceciliagerlach <gerlachcecilia@gmail.com>
2025-03-25 14:38:28 -0500

    Fixed my groups display

--- app/src/main/java/com/example/gustiehub/GlobalData.kt
+++ app/src/main/java/com/example/gustiehub/GlobalData.kt
@@ -69,6 +69,33 @@ object GlobalData {
             }
 
             snapshots?.let {
+                val updatedGroups = mutableListOf<Group>()
+                for (document in it.documents) {
+                    val group = document.toObject(Group::class.java)
+                    if (group != null) {
+                        if (userId in group.members) {
+                            updatedGroups.add(group)
+                        }
+                    }
+                }
+                println("Fetched ${updatedGroups.size} groups from Firestore.")
+                onGroupsUpdated(updatedGroups) // update views accordingly
+            }
+        }
+    }
+
+    // this was modified, so I changed the name instead of deleting
+    fun getPosts(userId: String, onGroupsUpdated: (List<Group>) -> Unit) {
+        val db = FirebaseFirestore.getInstance()
+        val groupsRef = db.collection("groups")
+
+        groupsRef.addSnapshotListener { snapshots, e ->
+            if (e != null) {
+                println("Error listening for group changes: ${e.message}")
+                return@addSnapshotListener
+            }
+
+            snapshots?.let {
                 val updatedPosts = mutableListOf<Post>()
                 for (document in it.documents) {
                     val post = document.toObject(Post::class.java)
