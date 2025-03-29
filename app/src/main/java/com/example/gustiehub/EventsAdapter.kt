96cb6ad3aa0ab6759152efe0b60af3ffd8045bb0
ceciliagerlach <gerlachcecilia@gmail.com>
2025-03-29 14:22:40 -0500

    Event View
    Creating user interface for event view and implemented recycler view

---
+++ app/src/main/java/com/example/gustiehub/EventsAdapter.kt
@@ -0,0 +1,43 @@
+package com.example.gustiehub
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+class EventsAdapter(
+    private var eventList: List<Event>
+) : RecyclerView.Adapter<EventsAdapter.EventsViewHolder>() {
+
+    class EventsViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val dateTextView: TextView = itemView.findViewById(R.id.event_date)
+        val timeTextView: TextView = itemView.findViewById(R.id.event_time)
+        val locationTextView: TextView = itemView.findViewById(R.id.event_location)
+        val nameTextView: TextView = itemView.findViewById(R.id.event_name)
+        val descriptionTextView: TextView = itemView.findViewById(R.id.event_description)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): EventsViewHolder {
+        val view = LayoutInflater.from(parent.context)
+            .inflate(R.layout.event_item, parent, false)
+        return EventsViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: EventsViewHolder, position: Int) {
+        val event = eventList[position]
+        holder.dateTextView.text = event.date
+        holder.timeTextView.text = event.time
+        holder.locationTextView.text = event.location
+        holder.nameTextView.text = event.eventName
+        holder.descriptionTextView.text = event.text
+    }
+
+    fun updateEvents(newEvents: List<Event>) {
+        eventList = newEvents
+        notifyDataSetChanged()
+    }
+
+    override fun getItemCount() = eventList.size
+}
