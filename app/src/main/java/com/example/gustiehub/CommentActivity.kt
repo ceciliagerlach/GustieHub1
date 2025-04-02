32aabf3ba8435c8900ef1a00a643f32bafc59ee1
ceciliagerlach <gerlachcecilia@gmail.com>
2025-04-02 16:14:21 -0500

    Toolbar functionality works from comment page

--- app/src/main/java/com/example/gustiehub/CommentActivity.kt
+++ app/src/main/java/com/example/gustiehub/CommentActivity.kt
@@ -1,16 +1,21 @@
 package com.example.gustiehub
 
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
 import android.widget.EditText
 import android.widget.ImageButton
+import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.GravityCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.navigation.NavigationView
 import com.google.firebase.Timestamp
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
@@ -21,6 +26,13 @@ class CommentActivity : AppCompatActivity() {
     private lateinit var commentInput: EditText
     private lateinit var commentButton: ImageButton
     private lateinit var postId: String
+    private lateinit var menuRecyclerView: RecyclerView
+    private lateinit var menuAdapter: MenuAdapter
+    private val groupList = mutableListOf<Group>()
+    private val filteredGroupList = mutableListOf<Group>()
+    // variables for toolbar and tabbed navigation
+    lateinit var navView: NavigationView
+    lateinit var drawerLayout: DrawerLayout
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -45,6 +57,73 @@ class CommentActivity : AppCompatActivity() {
         // Set onClickListener for comment submission
         commentButton.setOnClickListener {
             submitComment()
+        }
+
+        // list of groups in tab
+        val userID = FirebaseAuth.getInstance().currentUser?.uid ?: ""
+        menuRecyclerView = findViewById(R.id.recycler_menu)
+        menuRecyclerView.layoutManager = LinearLayoutManager(this)
+        menuAdapter = MenuAdapter(filteredGroupList) { selectedGroup ->
+            val intent = Intent(this, GroupPageActivity::class.java)
+            intent.putExtra("groupName", selectedGroup.name)
+            startActivity(intent)
+        }
+        menuRecyclerView.adapter = menuAdapter
+        GlobalData.getFilteredGroupList(userID){ updatedGroups ->
+            runOnUiThread {
+                groupList.clear()
+                groupList.addAll(updatedGroups)
+                menuAdapter.updateGroups(updatedGroups)
+            }
+        }
+
+        //set up drawer layout and handle clicks for menu items
+        navView = findViewById(R.id.nav_view)
+        drawerLayout = findViewById(R.id.tab_layout)
+        navView.setNavigationItemSelectedListener { menuItem ->
+            when (menuItem.itemId) {
+                R.id.dashboard -> {
+                    val intent = Intent(this, DashboardActivity::class.java)
+                    startActivity(intent)
+                }
+                R.id.announcements -> {
+                    val intent = Intent(this, AnnouncementsActivity::class.java)
+                    startActivity(intent)
+                }
+                R.id.marketplace -> {
+                    val intent = Intent(this, MarketplaceActivity::class.java)
+                    startActivity(intent)
+                }
+                R.id.events -> {
+                    val intent = Intent(this, EventsActivity::class.java)
+                    startActivity(intent)
+                }
+                R.id.groups -> {
+                    val intent = Intent(this, GroupsActivity::class.java)
+                    startActivity(intent)
+                }
+            }
+            drawerLayout.closeDrawer(GravityCompat.START)
+            true
+        }
+        
+        // opening menu
+        val menuButton: ImageView = findViewById(R.id.menu)
+        menuButton.setOnClickListener {
+            val drawerLayout = findViewById<DrawerLayout>(R.id.tab_layout)
+            drawerLayout.openDrawer(GravityCompat.START)
+        }
+
+        // handling clicks for toolbar
+        val messageButton: ImageView = findViewById(R.id.messaging)
+        val profileButton: ImageView = findViewById(R.id.profile)
+        messageButton.setOnClickListener {
+            val intent = Intent(this, MessageActivity::class.java)
+            startActivity(intent)
+        }
+        profileButton.setOnClickListener {
+            val intent = Intent(this, ProfileActivity::class.java)
+            startActivity(intent)
         }
     }
 
