7e1c50a21c35b35e8ba52d7cc561653790f61c80
Salem486 <104390262+Salem486@users.noreply.github.com>
2025-04-11 13:51:58 -0500

    creating items on marketplace is working

--- app/src/main/java/com/example/gustiehub/MarketplaceActivity.kt
+++ app/src/main/java/com/example/gustiehub/MarketplaceActivity.kt
@@ -1,8 +1,18 @@
 package com.example.gustiehub
 
+import android.app.AlertDialog
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ImageButton
 import android.widget.ImageView
+import android.widget.Toast
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.GravityCompat
 import androidx.drawerlayout.widget.DrawerLayout
@@ -10,19 +20,38 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.navigation.NavigationView
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.storage.FirebaseStorage
+import java.util.UUID
 
 class MarketplaceActivity: AppCompatActivity(){
     private lateinit var menuRecyclerView: RecyclerView
     private lateinit var menuAdapter: MenuAdapter
     private val groupList = mutableListOf<Group>()
     private val filteredGroupList = mutableListOf<Group>()
+    private lateinit var marketplaceAdapter: MarketplaceAdapter
+    private val itemList = mutableListOf<Marketplace>()
     // variables for toolbar and tabbed navigation
     lateinit var navView: NavigationView
     lateinit var drawerLayout: DrawerLayout
+    private lateinit var getContent: ActivityResultLauncher<String>
+    private var selectedPhotoUri: Uri? = null
+
+    val db = FirebaseFirestore.getInstance()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_marketplace)
+
+        val marketplaceRecyclerView = findViewById<RecyclerView>(R.id.marketplaceRecyclerView)
+        marketplaceRecyclerView.layoutManager = LinearLayoutManager(this)
+        marketplaceAdapter = MarketplaceAdapter(itemList)
+        marketplaceRecyclerView.adapter = marketplaceAdapter
+
+        getContent = registerForActivityResult(ActivityResultContracts.GetContent()) { uri: Uri? ->
+            selectedPhotoUri = uri
+        }
+        listenForItemsUpdate()
 
         // list of groups in tab
         val userID = FirebaseAuth.getInstance().currentUser?.uid ?: ""
@@ -88,6 +117,91 @@ class MarketplaceActivity: AppCompatActivity(){
         profileButton.setOnClickListener {
             val intent = Intent(this, ProfileActivity::class.java)
             startActivity(intent)
+        }
+
+        val createListingButton = findViewById<ImageButton>(R.id.make_listing_button)
+        createListingButton.setOnClickListener {
+            NewMarketItemDialog()
+        }
+}
+
+    private fun listenForItemsUpdate() {
+        db.collection("items")
+            .addSnapshotListener { snapshot, e ->
+                if (e != null) {
+                    Toast.makeText(this, "Error fetching events", Toast.LENGTH_SHORT).show()
+                    return@addSnapshotListener
+                }
+                itemList.clear()
+                for (document in snapshot!!.documents) {
+                    val item = document.toObject(Marketplace::class.java)
+                    if (item != null) {
+                        itemList.add(item)
+                    }
+                }
+                marketplaceAdapter.notifyDataSetChanged()
+            }
+    }
+
+    private fun NewMarketItemDialog() {
+        val dialogView = LayoutInflater.from(this).inflate(R.layout.new_listing_dialog, null)
+        val itemNameEditText = dialogView.findViewById<EditText>(R.id.newItemName)
+        val priceEditText = dialogView.findViewById<EditText>(R.id.newItemPrice)
+        val descriptionEditText = dialogView.findViewById<EditText>(R.id.newItemDescription)
+        val buttonCancel = dialogView.findViewById<Button>(R.id.buttonCancel)
+        val buttonConfirm = dialogView.findViewById<Button>(R.id.buttonConfirm)
+        val pickImageButton: Button = dialogView.findViewById(R.id.upload_a_picture)
+
+        val dialog = AlertDialog.Builder(this)
+            .setView(dialogView)
+            .setCancelable(true)
+            .create()
+
+        buttonCancel.setOnClickListener {
+            dialog.dismiss()
+        }
+        // Trigger the image picker when the button is clicked
+        pickImageButton.setOnClickListener {
+            getContent.launch("image/*")
+        }
+        buttonConfirm.setOnClickListener {
+            val itemName = itemNameEditText.text.toString()
+            val price = priceEditText.text.toString()
+            val description = descriptionEditText.text.toString()
+            if (itemName.isNotEmpty() && price.isNotEmpty() && description.isNotEmpty() && selectedPhotoUri != null) {
+                uploadItemAndCreateListing(itemName, price, description)
+                Toast.makeText(this, "Item Created: $itemName", Toast.LENGTH_SHORT).show()
+            } else {
+                Toast.makeText(this, "Please fill in all fields and select an image", Toast.LENGTH_LONG).show()
+                }
+        }
+        dialog.show()
+    }
+
+    fun uploadItemAndCreateListing(itemName: String, price: String, description: String) {
+        val storageRef =
+            FirebaseStorage.getInstance().reference.child("items/${UUID.randomUUID()}.jpg")
+
+        selectedPhotoUri?.let { uri ->
+            storageRef.putFile(uri)
+                .addOnSuccessListener {
+                    storageRef.downloadUrl.addOnSuccessListener { downloadUri ->
+                        val newItem = Marketplace(
+                            userName = FirebaseAuth.getInstance().currentUser?.displayName ?: "",
+                            userID = FirebaseAuth.getInstance().currentUser?.uid ?: "",
+                            itemName = itemName,
+                            price = price,
+                            description = description,
+                            itemPhotoURL = downloadUri.toString(),
+                            time = System.currentTimeMillis().toString(),
+                            itemID = UUID.randomUUID().toString()
+                        )
+                        newItem.createItemListing()
+                    }
+                }
+                .addOnFailureListener {
+                    Toast.makeText(this, "Image upload failed", Toast.LENGTH_SHORT).show()
+                }
         }
     }
 }
